2bd868e8d0e9d93cae1094fe85238c6446c1864a
danielaef7 <daniesc.07@hotmail.com>
2016-02-29 00:20:25 -0500

    filtrar ubicacion

--- src/main/resources/static/app/Filtrar/Filtrar.js
+++ src/main/resources/static/app/Filtrar/Filtrar.js
@@ -60,8 +60,33 @@ angular.module('myApp.vistaFiltrar', ['ngRoute'])
                      canchas.push(aux);
                  }
             } $scope.canchas=canchas;
-        });
-       
-        
+        });  
     };   
+     $scope.filtrarUbicacion = function(address){
+     var output = document.getElementById("map_canvas");
+        if (!navigator.geolocation){
+          output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+          return;
+        }
+
+        function success(position) {
+          var latitude  = position.coords.latitude;
+          var longitude = position.coords.longitude;
+
+          output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+          var img = new Image();
+          img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+          output.appendChild(img);
+        };
+
+        function error() {
+          output.innerHTML = "Unable to retrieve your location";
+        };
+
+        output.innerHTML = "<p>Locating…</p>";
+
+        navigator.geolocation.getCurrentPosition(success, error);
+     };
 }]);
