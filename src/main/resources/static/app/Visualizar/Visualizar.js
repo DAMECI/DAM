5d79adaf2b75d43076c7257e633dab590347707f
danielaef7 <daniesc.07@hotmail.com>
2016-02-29 18:40:49 -0500

    estilo y mapa

--- src/main/resources/static/app/Visualizar/Visualizar.js
+++ src/main/resources/static/app/Visualizar/Visualizar.js
@@ -15,7 +15,13 @@ angular.module('myApp.Visualizar', ['ngRoute'])
                 function simple() {
                    if (GBrowserIsCompatible()) {
                        var map = new GMap2(document.getElementById("map1"));
-                       map.setCenter(new GLatLng(4.6203798, -74.1096943), 11);
+                       map.setCenter(new GLatLng(40.466997, -3.705482), 11);
+                       map.setUIToDefault();
+                       
+                        var point = new GLatLng(4.710031, -74.077945);
+                        var marker = new GMarker(point);  
+                        map.addOverlay(marker);         
+                        marker.openInfoWindowHtml('Hello, <strong>World!</strong>');
                    }
                 }
                 window.onload = function () {
