948325b9d55b34d28d58110623727a30ffe62d6d
danielaef7 <daniesc.07@hotmail.com>
2016-03-01 00:25:28 -0500

    Capturar dirección

--- src/main/resources/static/app/Visualizar/Visualizar.js
+++ src/main/resources/static/app/Visualizar/Visualizar.js
@@ -15,27 +15,44 @@ angular.module('myApp.Visualizar', ['ngRoute'])
                 
                 function simple() {
                    if (GBrowserIsCompatible()) {
-                       var map = new GMap2(document.getElementById("map1"));
-                       map.setCenter(new GLatLng(4.6203798, -74.1096943), 11);
-                       map.setUIToDefault();
+                        var map = new GMap2(document.getElementById("map1"));
+                        map.setCenter(new GLatLng(4.6203798, -74.1096943), 11);
+                        map.setUIToDefault();
+                        var geocoder = new GClientGeocoder();
+                        for(var index = 0; index < $scope.establecimientos.length; index++) {   
+                            geocoder.getLatLng($scope.establecimientos[index].direccion, function(point){ 
+                                if (!point) { 
+                                  alert($scope.establecimientos[index].direccion + " not found"); 
+                                } 
+                                else { 
+                                     console.log("punto"+point);
+                                     return point; 
+                                } 
+                            }
+                        ); 
+                        //  geocoder.getLatLng($scope.establecimientos[index].direccion,geocodeResult);
+                        } 
+                        
+                   }
+                }
+                
+              /*  function geocodeResult(results, map) {
+                    var map2 = new GMap2(document.getElementById("map1"));
+                    var point = new GLatLng(4.6203798, -74.1096943);
+                    var marker = new GMarker(point);  
+                    map2.addOverlay(marker); 
                        var geocoder = new google.maps.Geocoder();
                        for(var index = 0; index < $scope.establecimientos.length; index++) {
                            geocoder.geocode({ 'address': $scope.establecimientos[index].direccion}, geocodeResult,map);
                            alert(index);
                         } 
-                   }
-                }
+                   }*/
                 
-                function geocodeResult(results, status,map) {
-                    var point = new GLatLng(4.6203798, -74.1096943);
-                    var marker = new GMarker(point);  
-                    map.addOverlay(marker); 
-                }
 
                 
                 window.onload = function () {
                    simple();
-                }                          
+                };                          
 
                
                console.log('Salio visualizacion');
