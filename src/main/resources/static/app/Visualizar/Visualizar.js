735e3e5bb27a72f6c73572125b845fb41afe6b2a
danielaef7 <daniesc.07@hotmail.com>
2016-03-01 14:27:51 -0500

    Marcadores establecimiento

--- src/main/resources/static/app/Visualizar/Visualizar.js
+++ src/main/resources/static/app/Visualizar/Visualizar.js
@@ -12,48 +12,32 @@ angular.module('myApp.Visualizar', ['ngRoute'])
                 console.log('Entro visualizacion');
         	$scope.establecimientos= Establecimiento.query();
                 google.load('maps', '2', {callback: simple});
+                var puntos=[];
                 
                 function simple() {
-                   if (GBrowserIsCompatible()) {
+                    if (GBrowserIsCompatible()) {
                         var map = new GMap2(document.getElementById("map1"));
                         map.setCenter(new GLatLng(4.6203798, -74.1096943), 11);
                         map.setUIToDefault();
                         var geocoder = new GClientGeocoder();
                         for(var index = 0; index < $scope.establecimientos.length; index++) {   
-                            geocoder.getLatLng($scope.establecimientos[index].direccion, function(point){ 
+                            geocoder.getLatLng($scope.establecimientos[index].direccion+"Bogota", function(point){ 
                                 if (!point) { 
                                   alert($scope.establecimientos[index].direccion + " not found"); 
                                 } 
                                 else { 
-                                     console.log("punto"+point);
-                                     return point; 
+                                    var marker = new GMarker (new GLatLng(point.lat(), point.lng())); 
+                                    map.addOverlay(marker);
+                                    console.log("puntoAgregado1");
+                                    return point; 
                                 } 
-                            }
-                        ); 
-                        //  geocoder.getLatLng($scope.establecimientos[index].direccion,geocodeResult);
+                            });                     
                         } 
-                        
-                   }
-                }
-                
-              /*  function geocodeResult(results, map) {
-                    var map2 = new GMap2(document.getElementById("map1"));
-                    var point = new GLatLng(4.6203798, -74.1096943);
-                    var marker = new GMarker(point);  
-                    map2.addOverlay(marker); 
-                       var geocoder = new google.maps.Geocoder();
-                       for(var index = 0; index < $scope.establecimientos.length; index++) {
-                           geocoder.geocode({ 'address': $scope.establecimientos[index].direccion}, geocodeResult,map);
-                           alert(index);
-                        } 
-                   }*/
-                
-
-                
+                    
+                    }
+                }           
                 window.onload = function () {
                    simple();
-                };                          
-
-               
+                };                                      
                console.log('Salio visualizacion');
 }]);
