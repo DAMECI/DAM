16e8bdf8245d42385bc41e539fa909baaf41267b
Paola Castro <ipao526@gmail.com>
2016-04-10 08:08:30 -0500

    cambio stub establecimienti

--- src/main/java/edu/eci/cosw/main/StubEstablecimiento.java
+++ src/main/java/edu/eci/cosw/main/StubEstablecimiento.java
@@ -24,7 +24,7 @@ import java.util.Set;
 public class StubEstablecimiento implements OperacionEstablecimiento{
     private List<Establecimiento>  establecimientos = new ArrayList<Establecimiento>();
     {
-<<<<<<< HEAD
+
         
         
         Set<Cancha> canchasEs1 = new HashSet(0);
@@ -36,10 +36,10 @@ public class StubEstablecimiento implements OperacionEstablecimiento{
         Set<Cancha> canchasEs3 = new HashSet(0);
 //        canchasEs3.add(new Cancha((new CanchasId(31,"8954-0")),true,3562));
 //        canchasEs3.add(new Cancha((new CanchasId(32,"8954-0")),false,3563));
-        establecimientos.add(new Establecimiento("1542-4","Paola", "Cancha5", "calle 170 #13-1", "12333",4.2,canchasEs1));
-        establecimientos.add(new Establecimiento("2541-1","Diego", "RealMadrid", "cra 1 #1-1", "12333",3.8, canchasEs2));
-        establecimientos.add(new Establecimiento("8954-0","Alejo", "Emirates", "cra 20 #2-2", "12333",4.2, canchasEs3));
-=======
+//        establecimientos.add(new Establecimiento("1542-4","Paola", "Cancha5", "calle 170 #13-1", "12333",4.2,canchasEs1));
+//        establecimientos.add(new Establecimiento("2541-1","Diego", "RealMadrid", "cra 1 #1-1", "12333",3.8, canchasEs2));
+//        establecimientos.add(new Establecimiento("8954-0","Alejo", "Emirates", "cra 20 #2-2", "12333",4.2, canchasEs3));
+
         //Set<Cancha> canchasEs1 = new HashSet(0);
         //canchasEs1.add(new Cancha((new CanchasId(11,"1542-4")),true,1562));
         //canchasEs1.add(new Cancha((new CanchasId(12,"1542-4")),false,1563));
@@ -52,7 +52,7 @@ public class StubEstablecimiento implements OperacionEstablecimiento{
         //establecimientos.add(new Establecimiento("1542-4","Paola", "Cancha5", "calle 170 #13-1", "12333",4.2,canchasEs1));
         //establecimientos.add(new Establecimiento("2541-1","Diego", "RealMadrid", "cra 1 #1-1", "12333",3.8, canchasEs2));
         //establecimientos.add(new Establecimiento("8954-0","Alejo", "Emirates", "cra 20 #2-2", "12333",4.2, canchasEs3));
->>>>>>> 75b7c4e3837a2d0028b69fe8ad2e39e332f23fe4
+
 
        
         
