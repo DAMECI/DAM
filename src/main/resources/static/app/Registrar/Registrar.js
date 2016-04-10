a2ebec1dec362be476b8a343bbcb60c526d52b0b
Paola Castro <ipao526@gmail.com>
2016-04-10 09:11:05 -0500

    Paquete repositorios

--- src/main/resources/static/app/Registrar/Registrar.js
+++ src/main/resources/static/app/Registrar/Registrar.js
@@ -17,18 +17,11 @@ angular.module('myApp.vistaRegistrar', ['ngRoute'])
     $scope.email ="";
     $scope.password ="";                
     $scope.agregarUsuario = function(){
+        console.log("Entro");
         var user = {"idCliente":$scope.idCliente,"nombre":$scope.Nombre, "apellido":$scope.Apellido, 
             "telefono":$scope.Telefono, "email":$scope.Email, "password":$scope.Password};
         usersService.save(user,function(){
-                var visualizar = document.getElementById('Visualizar');
-                var logOut = document.getElementById('Logout');
-                var logIn = document.getElementById('Login');
-                var registrar = document.getElementById('Registrar');
-                console.info("usuario Agregado");
-                visualizar.style.visibility = 'visible';
-                logOut.style.visibility = 'visible';
-                logIn.style.visibility = 'hidden';
-                registrar.style.visibility = 'hidden';
+                 
                 $location.path("/Visualizar");
         });
     };
