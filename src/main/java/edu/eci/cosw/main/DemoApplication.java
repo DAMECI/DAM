20830784b2b2be1bf1f8fea8b248d6088b5b31c7
danielaef7 <daniesc.07@hotmail.com>
2016-04-10 10:28:47 -0500

    main

--- src/main/java/edu/eci/cosw/main/DemoApplication.java
+++ src/main/java/edu/eci/cosw/main/DemoApplication.java
@@ -14,9 +14,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,6 +33,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
 
 @SpringBootApplication
+@EnableJpaRepositories("edu.eci.cosw.Repositories")
+@EntityScan("edu.eci.cosw.clases")
 public class DemoApplication {
 
 	public static void main(String[] args) {
