18dd72e39c513ddb62693596ee4c91140f3fa4e7
Paola Castro <ipao526@gmail.com>
2016-05-20 23:13:23 -0500

    color calificar

--- src/main/resources/static/app/Calificar/Calificar.js
+++ src/main/resources/static/app/Calificar/Calificar.js
@@ -37,19 +37,19 @@ angular.module('myApp.Calificar', ['ngRoute'])
        
        $scope.Calificacion = function () {
            
-           if(document.getElementById("star-1").checked = true){
+           if(document.getElementById("star-1").checked == true){
                 $scope.calificacion = 1;
            }
-           if(document.getElementById("star-2").checked = true){
+           if(document.getElementById("star-2").checked == true){
                $scope.calificacion = 2;
            }
-           if(document.getElementById("star-3").checked = true){
+           if(document.getElementById("star-3").checked == true){
                $scope.calificacion = 3;
            }
-           if(document.getElementById("star-4").checked = true){
+           if(document.getElementById("star-4").checked == true){
                $scope.calificacion = 4;
            }
-           if(document.getElementById("star-5").checked = true){
+           if(document.getElementById("star-5").checked == true){
                $scope.calificacion = 5;
            }
            
