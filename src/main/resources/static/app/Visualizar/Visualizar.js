f2885baea15339889e78c075004d6ee1488ba9aa
Paola Castro <ipao526@gmail.com>
2016-05-20 20:41:57 -0500

    modificando reservas agregando tabla

--- src/main/resources/static/app/Visualizar/Visualizar.js
+++ src/main/resources/static/app/Visualizar/Visualizar.js
@@ -30,8 +30,6 @@ angular.module('myApp.Visualizar', ['ngRoute'])
                     function simple() {
                        var btn= document.getElementById("Botones");
                        btn.style.visibility="visible";
-                       var btn= document.getElementById("Menu");
-                       btn.style.visibility="Hidden";
                         if (GBrowserIsCompatible()) {
                             var map = new GMap2(document.getElementById("map1"));
                             map.setCenter(new GLatLng(4.6203798, -74.1096943), 11);
